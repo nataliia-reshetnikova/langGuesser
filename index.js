105a1dc249c8ad8ac9a49cacda0fa6357ca98d84
nataliia-reshetnikova <nataliia.reshetnikova@mitel.com>
2023-09-06 12:50:15 -0400

    Print language title and color code it

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import { franc } from "franc";
+import { iso6393 } from "iso-639-3";
+import colors from "colors";
 
 const input = process.argv[2];
+const isoLangCode = franc(input);
 
-console.log(franc());
+const language = iso6393.find((element) => element["iso6393"] === isoLangCode);
+if (!!language) {
+  console.log(`Our best guess is: ${language.name}`.rainbow);
+} else {
+  console.log(`Sorry, try with another sample text`.bgRed);
+}
